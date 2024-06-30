bbd0571c9da16f4cd53ded11446ddba587a9285f
Amartya Giri <amartya.giri.9@gmail.com>
2024-06-30 17:37:39 +0530

    test cases added for MagicalArena module

--- test/MagicalArena.test.js
+++ test/MagicalArena.test.js
@@ -23,4 +23,33 @@ describe("MagicalArena", () => {
     expect(arena.playerB).toBe(playerB);
     expect(arena.currentTurn).toBe(playerA);
   });
+
+  test("Switch Turn", () => {
+    arena.switchTurn();
+    expect(arena.currentTurn).toBe(playerB);
+    arena.switchTurn();
+    expect(arena.currentTurn).toBe(playerA);
+  });
+
+  test("Play Turn", () => {
+    const spy = jest.spyOn(arena, "askForRoll");
+    playerA.attackPower = jest.fn(() => 10);
+    playerB.defencePower = jest.fn(() => 5);
+    playerB.reduceHealth = jest.fn();
+    arena.playTurn();
+    expect(playerB.reduceHealth).toHaveBeenCalledWith(5);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test("Ask for Roll", () => {
+    const spy = jest.spyOn(arena.rl, "question");
+    arena.askForRoll();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test("Start Combat", () => {
+    const spy = jest.spyOn(arena, "askForRoll");
+    arena.startCombat();
+    expect(spy).toHaveBeenCalled();
+  });
 });
